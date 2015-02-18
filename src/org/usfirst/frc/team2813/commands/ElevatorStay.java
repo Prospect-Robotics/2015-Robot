cc4a7a97c4ea7df10a861772b99bea376589c0b9
prospectrobotics <prospectrobotics2813@gmail.com>
2015-02-17 19:03:07 -0800

    Added Intake Commands

---
+++ src/org/usfirst/frc/team2813/commands/ElevatorStay.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team2813.commands;
+
+import org.usfirst.frc.team2813.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ElevatorStay extends Command {
+	boolean done = false;
+    public ElevatorStay() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.elevator.setSetpoint(Robot.elevator.returnEncoder());
+    	done = true;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	done = false;
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team2813/commands/IntakeOpen.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team2813.commands;
+
+import org.usfirst.frc.team2813.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class IntakeOpen extends Command {
+	
+	double leftOpen = 1.0;
+	double rightOpen = 0;
+
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void execute() {
+		// TODO Auto-generated method stub
+		Robot.intakePivot.pivotOut();
+		if (Robot.intakePivot.getPosition() == leftOpen){
+			Robot.intakePivot.pivotStop();
+			
+		}
+		
+		
+	}
+
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
